9a15418a7fc393c5a7217fee621d823d262ca54b
fajlu <fajlurahaman185@gmail.com>
2026-02-22 18:08:19 +0530

    "update"

---
+++ JavaScript Practice Question/test.js
@@ -0,0 +1,26 @@
+const myArr = [1,2,3,4,5,6,7]
+// myArr.push(6.32)
+// myArr.push('8');
+myArr.pop();
+// console.log(myArr);
+// console.log(myArr.length);
+// console.log(typeof(myArr));
+// console.log(myArr[9]);
+
+const myArr3 = [1,2,3,5,9,6];
+// console.log(myArr3[2]);
+// myArr3.push(Infinity);
+// console.log(myArr3);
+
+
+const myArr2 = new Array(5);// two empty elements
+myArr2[0] = '1';
+myArr2[1] = '6';
+myArr2[2] = '3';
+console.log(myArr2);
+
+const myArr4 = []
+myArr4.push('1');
+myArr4.push('3');
+myArr4.push('4');
+// console.log(typeof(myArr4));
